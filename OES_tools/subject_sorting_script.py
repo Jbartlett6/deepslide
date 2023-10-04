b1c57c4e03f641c2269da273fa1644b2bd56146d
Joseph Bartlett <j_j_bartlett@hotmail.co.uk>
2023-10-04 15:44:57 +1100

    Adding the option to include Mandard 3 in the all_wsi folder.

--- OES_tools/subject_sorting_script.py
+++ OES_tools/subject_sorting_script.py
@@ -3,30 +3,30 @@ import pandas as pd
 import os
 import glob
 
-def construct_class_dict(csv_path, mode):
+def construct_class_dict(csv_path, mode, include_M_3 = True):
     
     data = pd.read_csv(csv_path)
     class_dict = {}
+    
     if mode == 'all':
         vals = [1,2,3,5,6]
         classes = data['Mandard'].unique()[vals]
-
+        print(classes)
         
         for class_name in classes:
             class_dict[class_name] = list(data.query(f"Mandard=='{class_name}'")['record_id'])
 
     elif mode == 'bin':
-        classes = ['True', 'False']
-        pos_vals = [3,5]
-        neg_vals = [1,6]
 
-        pos_classes = data['Mandard'].unique()[pos_vals]
-        neg_classes = data['Mandard'].unique()[neg_vals]
-
-        pos_classes_list = list(data.query(f"Mandard=='{pos_classes[0]}'")['record_id']) + list(data.query(f"Mandard=='{pos_classes[1]}'")['record_id'])
-        neg_classes_list = list(data.query(f"Mandard=='{neg_classes[0]}'")['record_id']) + list(data.query(f"Mandard=='{neg_classes[1]}'")['record_id'])
-
-        class_dict = {'True': pos_classes_list, 'False': neg_classes_list}
+        true_query = "Mandard=='I - No residual cancer' | Mandard=='II - rare residual cancer cells'"
+        
+        if include_M_3 == True:
+            false_query = "Mandard=='III - Fibrosis outgrowing residual cancer' | Mandard=='IV - Residual cancer outgrowing fibrosis' | Mandard=='V - Absence of regressive changes'"
+        else:
+            false_query = "Mandard=='IV - Residual cancer outgrowing fibrosis' | Mandard=='V - Absence of regressive changes'"
+        
+        class_dict['True'] = list(data.query(true_query)['record_id'])
+        class_dict['False'] = list(data.query(false_query)['record_id'])
 
     return class_dict
 
@@ -45,18 +45,18 @@ def sort_subjects(class_dict, origin_dir, target_dir):
             
             for file in file_list:
                 shutil.copyfile(file, os.path.join(target_dir, class_name, file.split('/')[-1]))
-                # print(file.split('/')[-1])
-                # print(os.path.join(target_dir, class_name, file.split('/')[-1]))
-
 
 if __name__ == '__main__':
     deepslide_dir = '/data/gpfs/projects/punim2070/code/deepslide/deepslide'
     csv_path = os.path.join(deepslide_dir, 'OES_tools', 'Slide Scanning Log.xlsx - Labelling.csv')
-    target_dir = os.path.join(deepslide_dir, 'all_wsi')
+    target_dir = os.path.join(deepslide_dir, 'all_wsi_including_3')
+
+    assert os.path.exists(target_dir) == False, 'Target directory already exists, either delete current directory or change target directory'
     
+    os.mkdir(target_dir)
+
     origin_dir = '/data/gpfs/projects/punim2070/data_jpg/OAC Slides JPEG Export_downsampled10'
     mode = 'bin'
-    print(os.path.exists(target_dir), '\n', os.path.exists(csv_path), '\n', os.path.exists(origin_dir), '\n')
 
     class_dict = construct_class_dict(csv_path, mode)
     make_directories(class_dict, target_dir)
